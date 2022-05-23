55babf1294e486ab1860a35995dc37a4323797b0
Narmadha27 <106029748+Narmadha27@users.noreply.github.com>
2022-05-23 14:34:07 +0530

    Add files via upload

---
+++ Project/Static/JS/translate.js
@@ -0,0 +1,41 @@
+let input=document.querySelector('input');
+let textarea=document.querySelector('.target');
+let btn=document.getElementById("btnsearch");
+btn.addEventListener("click",doFindandReplace);
+input.addEventListener('change',()=>{
+    let files = input.files;
+    let file= files[0];
+    let reader=new FileReader();
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        textarea.value=lines.join('\n');
+    };
+    reader.readAsText(file);
+    
+});
+function doFindandReplace(){
+let len=english.length;
+for(let i=0;i<len;i++){
+let regexp=new RegExp(english[i],"ig");
+textarea.value=textarea.value.replace(regexp,french[i]); 
+}
+}
+function count(){
+    let textarea=document.querySelector("textarea");
+    let str=textarea.value;
+    let arrstr=str.split(" ");
+    let count=0;
+    let len=arrstr.length;
+    let input=document.getElementById("inputtext").value;
+    let regexp=new RegExp(input,"ig");
+    for(let i=0;i<len;i++){
+        if(arrstr[i].match(regexp)){
+            count=count+1;
+        }
+    }
+    alert(count)
+}
+
+
+
