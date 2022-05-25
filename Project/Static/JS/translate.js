dd3a635e57d7efe2312c4a62923e343f0e297646
Narmadha27 <106029748+Narmadha27@users.noreply.github.com>
2022-05-25 12:04:23 +0530

    Update translate.js

--- Project/Static/JS/translate.js
+++ Project/Static/JS/translate.js
@@ -1,26 +1,24 @@
 let input=document.querySelector('input');
 let textarea=document.querySelector('.target');
 let btn=document.getElementById("btnsearch");
-btn.addEventListener("click",doFindandReplace);
+btn.addEventListener("click",()=>{
+    let len=english.length;
+    for(let i=0;i<len;i++){
+        let regexp=new RegExp(english[i],"ig");
+        textarea.value=textarea.value.replace(regexp,french[i]); 
+    }
+};
 input.addEventListener('change',()=>{
     let files = input.files;
     let file= files[0];
     let reader=new FileReader();
     reader.onload = (e) => {
         const file = e.target.result;
-        const lines = file.split(/\r\n|\n/);
+        const lines = file.split(/\n/);
         textarea.value=lines.join('\n');
     };
-    reader.readAsText(file);
-    
+    reader.readAsText(file);    
 });
-function doFindandReplace(){
-let len=english.length;
-for(let i=0;i<len;i++){
-let regexp=new RegExp(english[i],"ig");
-textarea.value=textarea.value.replace(regexp,french[i]); 
-}
-}
 function count(){
     let textarea=document.querySelector("textarea");
     let str=textarea.value;
@@ -28,9 +26,8 @@ function count(){
     let count=0;
     let len=arrstr.length;
     let input=document.getElementById("inputtext").value;
-    let regexp=new RegExp(input,"ig");
     for(let i=0;i<len;i++){
-        if(arrstr[i].match(regexp)){
+        if(arrstr[i].match(input) && arrstr[i].length==input.length){
             count=count+1;
         }
     }
